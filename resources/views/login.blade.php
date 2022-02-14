d73660d1e02ddabb7b2c366993ee4cc19abebfe9
Wasee Sarwar <waseesarwar187@gmail.com>
2022-02-14 23:49:01 +0600

    login view setup

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,24 @@
 @extends('master')
 @section('content')
-<h1>Login Here</h1>
-<button class="btn btn-primary">Click Here</button>
+<div class="container d-flex justify-content-center custom-login-form">
+    <div class="row">
+        <h2 class="login-h mt-3">Login</h2>
+        <div class="col-lg-12">
+            <form>
+                <div class="form-group">
+                  <label for="inputEmail4" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="inputEmail4">
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="inputPassword4">
+                </div>
+            
+                <div class="col-12 mt-3">
+                  <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+              </form>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,24 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>E-comm Project</title>
-    
+    <!-- CSS Files-->
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="  https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    
+    <!-- Custom CSS Files-->
+    <link rel="stylesheet" href="{{url('css/footer.css')}}" />
+    <link rel="stylesheet" href="{{url('css/header.css')}}" />
+    <link rel="stylesheet" href="{{url('css/global.css')}}" />
+    <!-- JQUERY CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+    
+    <script src="https://kit.fontawesome.com/5e5998b57b.js" crossorigin="anonymous"></script>
+    </head>
 <body>
+    @include('header')
+    @yield('content')
+    @include('footer')
     
 </body>
-<script>
-    $(document).ready(function(){
-        $("button").click(function(){
-            alert('All set')
-        })
-    })
-</script>
 </html>
